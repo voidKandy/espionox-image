fb9dafb5db91865fc4b86c6bbfbd6a779bfac631
voidKandy <voidkandy@gmail.com>
2023-10-10 17:24:28 -0700

    image working

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,14 @@
 FROM postgres:13
 
-# COPY .env /etc/postgresql/.env
-
 COPY init_db.sh /docker-entrypoint-initdb.d/
+# COPY create_db.sql /docker-entrypoint-initdb.d/
+# COPY configure_db.sh /docker-entrypoint-initdb.d/
+
+COPY postgresql.conf /etc/postgresql/postgresql.conf
 
 WORKDIR /migrations
 
 COPY migrations/*.sql ./
+EXPOSE 5432
 
-EXPOSE 6987
-
-CMD ["postgres"]
+CMD ["postgres", "-c", "config_file=/etc/postgresql/postgresql.conf"]
--- init_db.sh
+++ init_db.sh
@@ -3,15 +3,29 @@
 # Use environment variables or set defaults
 : "${RESET_DB:=false}"
 : "${POSTGRES_DB:=espionox}"
-: "${POSTGRES_USER:=dev}"
+: "${POSTGRES_USER:=root}"
 : "${POSTGRES_PASSWORD:=strawhat.jury.steamboat.mother}"
+
+DB_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${HOST}:${PORT}/${POSTGRES_DB}"
+
+# Function to check if PostgreSQL is ready
+wait_for_postgres() {
+    until psql -h "${HOST}" -p "${PORT}" -U "${POSTGRES_USER}" -c '\q'; do
+        echo "PostgreSQL is unavailable - sleeping"
+        sleep 1
+    done
+    echo "PostgreSQL is up - executing command"
+}
+
+# Wait for PostgreSQL to be ready
+# wait_for_postgres
 
 if [ "${RESET_DB}" = "true" ]; then
     # Execute psql commands to drop existing database, create a new one, and create user
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    echo "Creating database: $POSTGRES_DB with User: $POSTGRES_USER and Password: $POSTGRES_PASSWORD" 
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
         DROP DATABASE IF EXISTS "$POSTGRES_DB";
         CREATE DATABASE "$POSTGRES_DB";
-        CREATE USER "$POSTGRES_USER" WITH PASSWORD '$POSTGRES_PASSWORD';
         ALTER DATABASE "$POSTGRES_DB" OWNER TO "$POSTGRES_USER";
 EOSQL
 
@@ -20,4 +34,4 @@ else
     echo "Database reset skipped. Use RESET_DB=true to reset the database."
 fi
 
-# Add your other schema setup statements here
+         # CREATE USER "$POSTGRES_USER" WITH PASSWORD '$POSTGRES_PASSWORD';
